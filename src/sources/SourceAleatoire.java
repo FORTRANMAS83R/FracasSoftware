eb73bffd72b10133a8ac8f685775445d4916f1a0
ByWezZ <valentin_clm@outlook.fr>
2024-09-12 11:23:23 +0200

    Source: genInformationAleatoire & genInformation

--- src/sources/SourceAleatoire.java
+++ src/sources/SourceAleatoire.java
@@ -1,17 +1,8 @@
 package sources;
 
-import java.util.Random;
-
-import information.Information;
-
-
 public class SourceAleatoire extends Source<Boolean> {
-    public SourceAleatoire(Integer nbBitsMess, Integer seed) {
-        super();
-        informationGeneree = new Information<>();
-        Random rdm = seed == null ? new Random() : new Random(seed);
-        for (int i = 0; i < nbBitsMess; i++) {
-            informationGeneree.add(rdm.nextBoolean());
-        }
-    }
+	public SourceAleatoire(Integer nbBitsMess, Integer seed) {
+		super();
+		informationGeneree = genInformationAleatoire(nbBitsMess, seed);
+	}
 }
--- src/sources/SourceFixe.java
+++ src/sources/SourceFixe.java
@@ -1,16 +1,10 @@
 package sources;
 
-import information.Information;
-
 public class SourceFixe extends Source<Boolean> {
 
-    public SourceFixe(String message) {
-        super();
-        informationGeneree = new Information<>();
-        for (int i = 0; i < message.length(); i++) {
-            if (message.charAt(i) == '0') informationGeneree.add(false);
-            else if (message.charAt(i) == '1') informationGeneree.add(true);
-        }
-    }
+	public SourceFixe(String message) {
+		super();
+		informationGeneree = genInformation(message);
+	}
 
 }
