911d84e74fe94302f5faebff3f61532da8c0a885
Gargamel <mikael.franco@imt-atlantique.fr>
2024-09-13 15:11:32 +0200

    add: partie de test RZ

--- tests/sources/SourceRZTest.java
+++ tests/sources/SourceRZTest.java
@@ -3,6 +3,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import sources.analogique.*;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import static org.hamcrest.CoreMatchers.is;
 
 public class SourceRZTest {
@@ -10,9 +14,9 @@ public class SourceRZTest {
     public ErrorCollector collector = new ErrorCollector();
 
     @Test
-    public void testSourceRZRFixe(){
-       SourceRZ src = new SourceRZ("10100110", 10, -5, 5);
-       collector.checkThat(src.getInformationGeneree().nbElements(),is(10*8));
+    public void testSourceRZRFixe() {
+        SourceRZ src = new SourceRZ("10100110", 10, -5, 5);
+        collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
         for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
             if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
                 collector.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
@@ -31,6 +35,7 @@ public class SourceRZTest {
             }
         }
     }
+
     @Test
     public void testSourceRZRandomSansSeed() {
         SourceRZ src = new SourceRZ(10, -5, 5, 8, null);
@@ -40,5 +45,24 @@ public class SourceRZTest {
                 collector.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
             }
         }
+    }
+    @Test
+    public void testInformationGeneree() {
+        SourceRZ test = new SourceRZ("010011", 10, -5, 5);
+        LinkedList<Float> expected = new LinkedList<>(Arrays.asList(-5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f,
+                -5.0f, -5.0f, -5.0f, -5.0f, 5.0f, 5.0f, 5.0f, -5.0f, -5.0f, -5.0f,
+                -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f,
+                -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f, -5.0f,
+                -5.0f, -5.0f, -5.0f, 5.0f, 5.0f, 5.0f, -5.0f, -5.0f, -5.0f, -5.0f,
+                -5.0f, -5.0f, -5.0f, 5.0f, 5.0f, 5.0f, -5.0f, -5.0f, -5.0f));
+        for (int i = 0; i < test.getInformationGeneree().nbElements(); i++) {
+            if (!test.getInformationGeneree().iemeElement(i).equals(expected.get(i))) {
+                System.out.println(test.getInformationGeneree().iemeElement(i));
+                System.out.println(expected.get(i));
+                collector.addError(new AssertionError("Les valeurs générées ne sont pas correctes"));
+
+            }
+        }
+
     }
 }
