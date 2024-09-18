4f1ecb5644678a187b4ac01ce8d8100e304846c2
j23trone <j23trone@imt-atlantique.net>
2024-09-18 18:59:24 +0200

    Ajout commentaires sur ArgumentTest

--- tests/simulateur/ArgumentTest.java
+++ tests/simulateur/ArgumentTest.java
@@ -3,23 +3,36 @@ package simulateur;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
+/**
+ * Classe de test pour Argument.
+ * Utilise JUnit pour les assertions et ExpectedException pour gérer les exceptions attendues.
+ */
 public class ArgumentTest {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
-
+	/**
+	 * Teste la gestion d'une forme invalide.
+	 * @throws ArgumentsException si la forme est invalide.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testFormeInvalide() throws ArgumentsException {
 		final String[] args = { "-form", "TEST_INVALIDE" };
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude minimale supérieure à l'amplitude maximale.
+	 * @throws ArgumentsException si l'amplitude minimale est supérieure à l'amplitude maximale.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testAmplitudeMinSuperieurAMax() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "3", "1" };
 		new Simulateur(args);
 	}
 
+	/**
+	 * Teste la gestion d'une amplitude minimale égale à l'amplitude maximale.
+	 * @throws ArgumentsException si l'amplitude minimale est égale à l'amplitude maximale.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testAmplitudeMinEgaleAMax() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "3", "3" };
@@ -27,27 +40,40 @@ public class ArgumentTest {
 		new Simulateur(args);
 	}
 
+	/**
+	 * Teste la gestion d'une amplitude minimale différente de 0 pour la forme RZ.
+	 * @throws ArgumentsException si l'amplitude minimale est différente de 0 pour la forme RZ.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testRZAmplitudeMinDifferentDe0() throws ArgumentsException {
 		final String[] args = { "-form", "RZ", "-ampl", "-3", "3" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude maximale inférieure à 0 pour la forme NRZ.
+	 * @throws ArgumentsException si l'amplitude maximale est inférieure à 0 pour la forme NRZ.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testNRZAmplitudeMaxInferieurA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "-5", "-1.2" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude maximale inférieure à 0 pour la forme NRZT.
+	 * @throws ArgumentsException si l'amplitude maximale est inférieure à 0 pour la forme NRZT.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testNRZTAmplitudeMaxInferieurA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZT", "-ampl", "-5", "-1.2" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude minimale supérieure à 0 pour la forme NRZ.
+	 * @throws ArgumentsException si l'amplitude minimale est supérieure à 0 pour la forme NRZ.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testNRZAmplitudeMinSuperieurA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "0.3", "1.2" };
@@ -55,6 +81,10 @@ public class ArgumentTest {
 		new Simulateur(args);
 	}
 
+	/**
+	 * Teste la gestion d'une amplitude minimale supérieure à 0 pour la forme NRZT.
+	 * @throws ArgumentsException si l'amplitude minimale est supérieure à 0 pour la forme NRZT.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testNRZTAmplitudeMinSuperieurA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZT", "-ampl", "0.3", "1.2" };
@@ -62,20 +92,30 @@ public class ArgumentTest {
 		new Simulateur(args);
 	}
 
+	/**
+	 * Teste la gestion d'une amplitude minimale et maximale égale à 0 pour la forme NRZ.
+	 * @throws ArgumentsException si l'amplitude minimale et maximale est égale à 0 pour la forme NRZ.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testNRZAmplitudeMinMaxEgaleA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "0", "0" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude minimale et maximale égale à 0 pour la forme NRZT.
+	 * @throws ArgumentsException si l'amplitude minimale et maximale est égale à 0 pour la forme NRZT.
+	 */
 	@Test(expected = ArgumentsException.class)
 	public void testNRZTAmplitudeMinMaxEgaleA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZT", "-ampl", "0", "0" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude minimale erronée.
+	 * @throws ArgumentsException si l'amplitude minimale est erronée.
+	 */
 	@Test()
 	public void testAmplitudeMinErronee() throws ArgumentsException {
 		final String[] args = { "-ampl", "0@9", "0" };
@@ -86,6 +126,10 @@ public class ArgumentTest {
 		new Simulateur(args);
 	}
 
+	/**
+	 * Teste la gestion d'une amplitude maximale erronée.
+	 * @throws ArgumentsException si l'amplitude maximale est erronée.
+	 */
 	@Test()
 	public void testAmplitudeMaxErronee() throws ArgumentsException {
 		final String[] args = { "-ampl", "0", "0@9" };
@@ -96,27 +140,40 @@ public class ArgumentTest {
 		new Simulateur(args);
 	}
 
+	/**
+	 * Teste la gestion d'une amplitude minimale égale à 0 pour la forme NRZ.
+	 * @throws ArgumentsException si l'amplitude minimale est égale à 0 pour la forme NRZ.
+	 */
 	@Test()
 	public void testNRZAmplitudeMinEgaleA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "0", "1.2" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude minimale égale à 0 pour la forme NRZT.
+	 * @throws ArgumentsException si l'amplitude minimale est égale à 0 pour la forme NRZT.
+	 */
 	@Test()
 	public void testNRZTAmplitudeMinEgaleA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZT", "-ampl", "0", "1.2" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude maximale égale à 0 pour la forme NRZ.
+	 * @throws ArgumentsException si l'amplitude maximale est égale à 0 pour la forme NRZ.
+	 */
 	@Test()
 	public void testNRZAmplitudeMaxEgaleA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZ", "-ampl", "-1.2", "0" };
 
 		new Simulateur(args);
 	}
-
+	/**
+	 * Teste la gestion d'une amplitude maximale égale à 0 pour la forme NRZT.
+	 * @throws ArgumentsException si l'amplitude maximale est égale à 0 pour la forme NRZT.
+	 */
 	@Test()
 	public void testNRZTAmplitudeMaxEgaleA0() throws ArgumentsException {
 		final String[] args = { "-form", "NRZT", "-ampl", "-1.2", "0" };
