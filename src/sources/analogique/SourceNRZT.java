f696846809200222b0bb07efabf7529ad3b2139f
ByWezZ <valentin_clm@outlook.fr>
2024-09-22 18:44:56 +0200

    Fix NRZT source

--- src/sources/analogique/SourceNRZT.java
+++ src/sources/analogique/SourceNRZT.java
@@ -1,60 +1,68 @@
 package sources.analogique;
 
 /**
- * La classe SourceNRZT représente une source analogique utilisant le codage Non-Return-to-Zero Inversé (NRZT).
- * Elle étend la classe SourceAnalogique et implémente la méthode de filtrage spécifique pour le codage NRZT.
+ * La classe SourceNRZT représente une source analogique utilisant le codage
+ * Non-Return-to-Zero Inversé (NRZT). Elle étend la classe SourceAnalogique et
+ * implémente la méthode de filtrage spécifique pour le codage NRZT.
  */
 
 public class SourceNRZT extends SourceAnalogique {
-    /**
-     * Construit un objet SourceNRZT avec le message et les paramètres spécifiés.
-     *
-     * @param message       Le message à encoder.
-     * @param nbEchantillon Le nombre d'échantillons par symbole.
-     * @param amp_min       L'amplitude minimale.
-     * @param amp_max       L'amplitude maximale.
-     */
-    public SourceNRZT(String message, int nbEchantillon, float amp_min, float amp_max) {
-        super(message, nbEchantillon, amp_min, amp_max);
-    }
+	/**
+	 * Construit un objet SourceNRZT avec le message et les paramètres spécifiés.
+	 *
+	 * @param message       Le message à encoder.
+	 * @param nbEchantillon Le nombre d'échantillons par symbole.
+	 * @param amp_min       L'amplitude minimale.
+	 * @param amp_max       L'amplitude maximale.
+	 */
+	public SourceNRZT(String message, int nbEchantillon, float amp_min, float amp_max) {
+		super(message, nbEchantillon, amp_min, amp_max);
+	}
 
-    /**
-     * Construit un objet SourceNRZT avec des paramètres de message générés aléatoirement.
-     *
-     * @param nbEchantillon Le nombre d'échantillons par symbole.
-     * @param amp_min       L'amplitude minimale.
-     * @param amp_max       L'amplitude maximale.
-     * @param nbBits        Le nombre de bits dans le message généré.
-     * @param seed          La graine pour la génération aléatoire (peut être null).
-     */
-    public SourceNRZT(int nbEchantillon, float amp_min, float amp_max, int nbBits, Integer seed) {
-        super(nbEchantillon, amp_min, amp_max, nbBits, seed);
-    }
+	/**
+	 * Construit un objet SourceNRZT avec des paramètres de message générés
+	 * aléatoirement.
+	 *
+	 * @param nbEchantillon Le nombre d'échantillons par symbole.
+	 * @param amp_min       L'amplitude minimale.
+	 * @param amp_max       L'amplitude maximale.
+	 * @param nbBits        Le nombre de bits dans le message généré.
+	 * @param seed          La graine pour la génération aléatoire (peut être null).
+	 */
+	public SourceNRZT(int nbEchantillon, float amp_min, float amp_max, int nbBits, Integer seed) {
+		super(nbEchantillon, amp_min, amp_max, nbBits, seed);
+	}
 
-    /**
-     * Implémente la méthode de filtrage spécifique pour le codage NRZT.
-     * Cette méthode traite les échantillons d'information générés.
-     */
-    @Override
-    protected void filtreMiseEnForme() {
-        informationGeneree = informationEchantillon.clone();
+	/**
+	 * Implémente la méthode de filtrage spécifique pour le codage NRZT. Cette
+	 * méthode traite les échantillons d'information générés.
+	 */
+	@Override
+	protected void filtreMiseEnForme() {
+		informationGeneree = informationEchantillon.clone();
 
-        for (int i = 0; i < informationEchantillon.nbElements(); i += nbEchantillon) {
-            final float symbole_actuel = informationEchantillon.iemeElement(i);
-            final float val_fin = informationEchantillon.nbElements() > i + nbEchantillon ? (informationEchantillon.iemeElement(i + nbEchantillon) + symbole_actuel) / 2 : 0;
-            final float val_depart = i == 0 ? 0 : (informationEchantillon.iemeElement(i - 1) + symbole_actuel) / 2;
-            final float delta_1er_tier = symbole_actuel == val_depart ? 0 : (symbole_actuel - val_depart) / ((float) nbEchantillon / 3f);
-            final float delta_3eme_tier = symbole_actuel == val_fin ? 0 : (val_fin - symbole_actuel) / (((float) nbEchantillon / 3f) - (i + nbEchantillon == informationEchantillon.nbElements() ? 1 : 0));
+		for (int i = 0; i < informationEchantillon.nbElements(); i += nbEchantillon) {
+			final float symbole_actuel = informationEchantillon.iemeElement(i);
+			final float val_fin = informationEchantillon.nbElements() > i + nbEchantillon
+					? (informationEchantillon.iemeElement(i + nbEchantillon) + symbole_actuel) / 2
+					: 0;
+			final float val_depart = i == 0 ? 0 : (informationEchantillon.iemeElement(i - 1) + symbole_actuel) / 2;
+			final float delta_1er_tier = symbole_actuel == val_depart ? 0
+					: symbole_actuel / ((float) nbEchantillon / 3f);
+			final float delta_3eme_tier = symbole_actuel == val_fin ? 0
+					: (0 - symbole_actuel) / (((float) nbEchantillon / 3f)
+							- (i + nbEchantillon == informationEchantillon.nbElements() ? 1 : 0));
 
-            if (delta_1er_tier != 0)
-                for (int j = 0; j < nbEchantillon / 3; j++) {
-                    informationGeneree.setIemeElement(i + j, val_depart + j * delta_1er_tier);
-                }
+			if (delta_1er_tier != 0)
+				for (int j = 0; j < nbEchantillon / 3; j++) {
+					informationGeneree.setIemeElement(i + j, j * delta_1er_tier);
+				}
 
-            if (delta_3eme_tier != 0)
-                for (int j = 0; j < nbEchantillon / 3; j++) {
-                    informationGeneree.setIemeElement(i + (2 * nbEchantillon / 3) + j, symbole_actuel + j * delta_3eme_tier);
-                }
-        }
-    }
+			if (delta_3eme_tier != 0)
+				for (int j = 0; j < nbEchantillon / 3; j++) {
+					informationGeneree.setIemeElement(i + (2 * nbEchantillon / 3) + j,
+							symbole_actuel + j * delta_3eme_tier);
+				}
+		}
+	}
 }
--- tests/sources/SourceNRZTTest.java
+++ tests/sources/SourceNRZTTest.java
@@ -1,86 +1,94 @@
 package sources;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ErrorCollector;
-import sources.analogique.SourceNRZT;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Objects;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.fail;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import sources.analogique.SourceNRZT;
 
 /**
- * Classe de test pour SourceNRZT.
- * Utilise JUnit pour les assertions et ErrorCollector pour collecter les erreurs.
+ * Classe de test pour SourceNRZT. Utilise JUnit pour les assertions et
+ * ErrorCollector pour collecter les erreurs.
  */
 public class SourceNRZTTest {
-    @Rule
-    public ErrorCollector collector = new ErrorCollector();
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
 
-    /**
-     * Teste la génération de la source NRZT avec un message fixe.
-     * Vérifie que le nombre d'éléments générés est correct et que les valeurs sont dans l'intervalle d'amplitude.
-     */
-    @Test
-    public void testNRZTFixe() {
-        SourceNRZT src = new SourceNRZT("10100110", 10, -5, 5);
-        collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
-        for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
-            if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
-                collector.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
-            }
-        }
-    }
+	/**
+	 * Teste la génération de la source NRZT avec un message fixe. Vérifie que le
+	 * nombre d'éléments générés est correct et que les valeurs sont dans
+	 * l'intervalle d'amplitude.
+	 */
+	@Test
+	public void testNRZTFixe() {
+		SourceNRZT src = new SourceNRZT("10100110", 10, -5, 5);
+		collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
+		for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
+			if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
+				collector
+						.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
+			}
+		}
+	}
 
+	/**
+	 * Teste la génération de la source NRZT avec des valeurs aléatoires et une
+	 * graine. Vérifie que le nombre d'éléments générés est correct et que les
+	 * valeurs sont dans l'intervalle d'amplitude.
+	 */
+	@Test
+	public void testNRZTRandomAvecSeed() {
+		SourceNRZT src = new SourceNRZT(10, -5, 5, 8, 10);
+		collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
+		for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
+			if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
+				collector
+						.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
+			}
+		}
+	}
 
-    /**
-     * Teste la génération de la source NRZT avec des valeurs aléatoires et une graine.
-     * Vérifie que le nombre d'éléments générés est correct et que les valeurs sont dans l'intervalle d'amplitude.
-     */
-    @Test
-    public void testNRZTRandomAvecSeed() {
-        SourceNRZT src = new SourceNRZT(10, -5, 5, 8, 10);
-        collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
-        for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
-            if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
-                collector.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
-            }
-        }
-    }
+	/**
+	 * Teste la génération de la source NRZT avec des valeurs aléatoires sans
+	 * graine. Vérifie que le nombre d'éléments générés est correct et que les
+	 * valeurs sont dans l'intervalle d'amplitude.
+	 */
+	@Test
+	public void testNRZTRandomSansSeed() {
+		SourceNRZT src = new SourceNRZT(10, -5, 5, 8, null);
+		collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
+		for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
+			if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
+				collector
+						.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
+			}
+		}
+	}
 
-    /**
-     * Teste la génération de la source NRZT avec des valeurs aléatoires sans graine.
-     * Vérifie que le nombre d'éléments générés est correct et que les valeurs sont dans l'intervalle d'amplitude.
-     */
-    @Test
-    public void testNRZTRandomSansSeed() {
-        SourceNRZT src = new SourceNRZT(10, -5, 5, 8, null);
-        collector.checkThat(src.getInformationGeneree().nbElements(), is(10 * 8));
-        for (int i = 0; i < src.getInformationGeneree().nbElements(); i++) {
-            if (src.getInformationGeneree().iemeElement(i) < -5 || src.getInformationGeneree().iemeElement(i) > 5) {
-                collector.addError(new AssertionError("Les valeurs générées ne sont pas dans l'intervalle d'amplitude"));
-            }
-        }
-    }
+	/**
+	 * Vérifie la véracité des informations générées en comparant avec les valeurs
+	 * attendues. Actuellement, ce test n'est pas implémenté et génère une erreur.
+	 */
+	@Test
+	public void testInformationGeneree() {
+		SourceNRZT test = new SourceNRZT("010011", 10, -2.5f, 5);
 
-    /**
-     * Vérifie la véracité des informations générées en comparant avec les valeurs attendues.
-     * Actuellement, ce test n'est pas implémenté et génère une erreur.
-     */
-    @Test
-    public void testInformationGeneree() {
-        SourceNRZT test = new SourceNRZT("010011", 10, -2.5f, 5);
-
-        LinkedList<Float> expected = new LinkedList<>(Arrays.asList(0.0f, -0.75f, -1.5f, -2.5f, -2.5f, -2.5f, -2.5f, -1.375f, -0.25f, -2.5f, 1.25f, 2.375f, 3.5f, 5.0f, 5.0f, 5.0f, 5.0f, 3.875f, 2.75f, 5.0f, 1.25f, 0.125f, -1.0f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -1.375f, -0.25f, -2.5f, 1.25f, 2.375f, 3.5f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 2.8571427f, 0.7142854f, 5.0f));
-        for (int i = 0; i < test.getInformationGeneree().nbElements(); i++) {
-            if (!Objects.equals(test.getInformationGeneree().iemeElement(i), expected.get(i))) {
-                fail("Erreur de génération de la source NRZT");
-            }
-        }
-
-//        collector.addError(new Error("Test non implemente"));
-    }
+		LinkedList<Float> expected = new LinkedList<>(Arrays.asList(-0.0f, -0.75f, -1.5f, -2.5f, -2.5f, -2.5f, -2.5f,
+				-1.75f, -1.0f, -2.5f, 0.0f, 1.5f, 3.0f, 5.0f, 5.0f, 5.0f, 5.0f, 3.5f, 2.0f, 5.0f, -0.0f, -0.75f, -1.5f,
+				-2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f, -2.5f,
+				-1.75f, -1.0f, -2.5f, 0.0f, 1.5f, 3.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f,
+				5.0f, 5.0f, 5.0f, 5.0f, 2.8571427f, 0.7142854f, 5.0f));
+		for (int i = 0; i < test.getInformationGeneree().nbElements(); i++) {
+			if (!Objects.equals(test.getInformationGeneree().iemeElement(i), expected.get(i))) {
+				fail("Erreur de génération de la source NRZT");
+			}
+		}
+	}
 }
