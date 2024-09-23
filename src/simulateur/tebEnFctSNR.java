3a946f79545986f1bee6fba5fc108e7482f0e473
WW9kYQ <maxence.huet2003@gmail.com>
2024-09-23 23:33:10 +0200

    Petits changements dans tebEnFctSNR.java

--- src/simulateur/tebEnFctSNR.java
+++ src/simulateur/tebEnFctSNR.java
@@ -12,11 +12,11 @@ public class tebEnFctSNR {
         LinkedList<float[]> tabs = new LinkedList<>();
         float[] finalTab = new float[size];
 
-        for (int j = 0; j <50; j++) {
+        for (int j = 0; j <5000; j++) {
             float[] tempTab = new float[size];
             int i = 0;
             for (double snr = -50; snr <= 30; snr += 0.1) {
-                Simulateur.main(new String[] {"-mess","100","-form","NRZ","-nbEch","30","-snrpb", String.valueOf(Math.round(snr * 1000)/(float)1000)});
+                Simulateur.main(new String[] {"-mess","100","-form","NRZ","-nbEch","3","-snrpb", String.valueOf(Math.round(snr * 1000)/(float)1000)});
                 tempTab[i] = Simulateur.TEB;
                 i++;
             }
