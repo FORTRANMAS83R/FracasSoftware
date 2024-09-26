0e86f98d466795c7c5b78a0545a93f059c656f20
WW9kYQ <maxence.huet2003@gmail.com>
2024-09-26 17:58:20 +0200

    Ajout de la librairie Common Math d'Apache pour le calcul du TEB théorique; modification de la classe tebEnFctSNR pour intégrer le calcul et l'affichage du TEB thoéorique
    # Conflicts:
    #	SIT123.iml

--- src/simulateur/tebEnFctSNR.java
+++ src/simulateur/tebEnFctSNR.java
@@ -1,5 +1,6 @@
 package simulateur;
 
+import org.apache.commons.math3.special.Erf;
 import visualisations.VueCourbe;
 
 import java.util.Arrays;
@@ -7,16 +8,19 @@ import java.util.LinkedList;
 
 public class tebEnFctSNR {
     public static void main(String[] args) throws Exception {
-        int size = 800; // si vous modifiez cette valeur, vérifiez que la boucle avec snr ne lance pas OutOfBandsException
+        int size = 800;     // si vous modifiez cette valeur, vérifiez que la boucle avec snr ne lance pas OutOfBandsException
+        double snrMin = -50;
+        double snrMax = 30;
 
         LinkedList<float[]> tabs = new LinkedList<>();
         float[] finalTab = new float[size];
+        float[] tabTEBTheo = new float[size];
         // Boucle pour effectuer plusieurs simulations
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 5000; j++) {
             float[] tempTab = new float[size];
             int i = 0;
             // Boucle pour varier le SNR de -50 à 30 avec un pas de 0.1
-            for (double snr = -50; snr <= 30; snr += 0.1) {
+            for (double snr = snrMin; snr <= snrMax; snr += 0.1) {
                 // Création et exécution du simulateur avec les paramètres spécifiés
                 Simulateur simulateur = new Simulateur(new String[]{"-mess", "100", "-form", "NRZ", "-nbEch", "3", "-snrpb", String.valueOf(Math.round(snr * 1000) / (float) 1000)});
                 simulateur.execute();
@@ -24,8 +28,15 @@ public class tebEnFctSNR {
                 tempTab[i] = simulateur.calculTauxErreurBinaire();
                 i++;
             }
+            System.out.println(j);
             tabs.add(tempTab);
         }
+        int i = 0;
+        for (double snr = snrMin; snr <= snrMax; snr += 0.1) {
+            tabTEBTheo[i] = (float) (0.5*Erf.erfc(Math.sqrt(Math.pow(10, snr/10))));
+            i++;
+        }
+
         // Calcul de la moyenne des taux d'erreur binaire pour chaque valeur de SNR
         for (int y = 0; y < size; y++) {
             float somme = 0;
@@ -35,7 +46,12 @@ public class tebEnFctSNR {
             finalTab[y] = somme / (float) tabs.size();
         }
         // Affichage des résultats finaux et création de la courbe
+//        System.out.println(Arrays.toString(finalTab));
         System.out.println(Arrays.toString(finalTab));
+        System.out.println(Arrays.toString(tabTEBTheo));
         new VueCourbe(finalTab, "TEB en fonction du SNR");
+        new VueCourbe(tabTEBTheo, "TEB théorique en fonction du SNR");
     }
+
+
 }
