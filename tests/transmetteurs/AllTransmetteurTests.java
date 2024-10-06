124c886763093b70474a6742a60ef42e8b29cbee
WW9kYQ <maxence.huet2003@gmail.com>
2024-10-06 20:53:47 +0200

    Suppression fichier inutiliÃsé

--- tests/transmetteurs/AllTransmetteurTests.java
+++ tests/transmetteurs/AllTransmetteurTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TransmetteurParfaitTest.class, TransmetteurMultiTrajetTest.class, ConvertisseurAnalogiqueNumeriqueTest.class, ConvertisseurNumeriqueAnalogiqueTest.class, DecodeurTest.class, EgaliseurTest.class, CodeurTest.class})
+@Suite.SuiteClasses({TransmetteurParfaitTest.class, TransmetteurMultiTrajetTest.class, ConvertisseurAnalogiqueNumeriqueTest.class, ConvertisseurNumeriqueAnalogiqueTest.class, DecodeurTest.class, CodeurTest.class})
 public class AllTransmetteurTests {
 }
